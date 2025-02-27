314433abbf3feb98a654690214d9b7d02cbceeda
Alexander Hartigan <alexandermhartigan@gmail.com>
2025-02-26 21:29:03 -0800

    set up environment

---
+++ router/routes.js
@@ -0,0 +1,9 @@
+import express from "express";
+// import {example} from "../controller/example.js"
+
+import router from express.Router();
+
+//add requests
+//EX: app.get("/", example)
+
+export default router;
